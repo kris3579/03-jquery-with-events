c4f1bb921104e4d0c6e3c3f116e0b258f03d42ec
Kris3579 <kristianesvelt@hotmail.com>
2018-08-16 18:08:11 -0700

    review edit

--- kris-heather/scripts/articleView.js
+++ kris-heather/scripts/articleView.js
@@ -75,7 +75,7 @@ articleView.handleCategoryFilter = function () {
 articleView.handleMainNav = function () {
   // TODO: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
-  $('.tab').toggleClass('tab-content').html().hide();
+  $('.tab').toggleClass('.tab-content').html().hide();
   $('.tab').toggleClass('.tab-content')
 
   //maybe use this from HTML: data-content=“articles”
@@ -93,10 +93,11 @@ articleView.setTeasers = function () {
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
   let addEventToArticles = $('#articles');
-  addEventToArticles.on('click', event => {
+  addEventToArticles.on('click', '.read-on', (event) => {
     event.preventDefault();
-    $('.article-body *:nth-of-type(n+2)').show();
-    $('.read-on').hide();
+    // $('.article-body *:nth-of-type(n+2)').show();
+    $(this).prev().find('p').show();
+    // $('.read-on').hide();
   });
 
 
